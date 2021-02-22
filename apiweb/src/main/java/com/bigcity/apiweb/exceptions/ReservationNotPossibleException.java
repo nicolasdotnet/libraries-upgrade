19334681b3aac8942e7a34f6b25727cb95b53e09
nicolasdotnet <nicolas.desdevises@gmail.com>
2021-02-22 17:59:53 +0100

    adding Exception  file  : reservation not possible

---
+++ apiweb/src/main/java/com/bigcity/apiweb/exceptions/ReservationNotPossibleException.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bigcity.apiweb.exceptions;
+
+/**
+ *
+ * @author pi
+ */
+public class ReservationNotPossibleException extends LibraryException {
+
+    public ReservationNotPossibleException() {
+    }
+
+    public ReservationNotPossibleException(String message) {
+        super(message);
+    }
+    
+    
+    
+}
